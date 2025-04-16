93041879a39242215f72bcfdfca9709c3c9d865b
Hady <itshadyy@gmail.com>
2025-04-16 21:39:47 +0200

    Events & About Us Pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ScrollToTop from "./components/ScrollToTop";
 import Footer from './components/Footer';
+import Events from './pages/Events';
+import About from './pages/About';
 
 function App() {
   return (
@@ -14,7 +16,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/events" element={<Events />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-function Home() {
+function About() {
     const [counters, setCounters] = useState({
         landUnder: 0,
         rawLand: 0,
@@ -65,10 +65,12 @@ function Home() {
                 <h1>Collection  <span>of</span>  Beautiful <br /> Destinations</h1>
             </div>
 
-            <div className="home02">
+            <div className="about01">
                 <h1>WHO WE ARE</h1>
                 <p>Roya Developments has always been crafting a space with intention and care, where each tailored element speaks to the story and needs of the dwellers within it. Since the company was founded in 2007, a name was made for itself for its genuine approach to creating inclusive, contemporary spaces that fuse timeless-ness and innovation within their many projects across Egypt.</p>
                 <p>Our wide portfolio of projects consists of Telal North Coast which is dotted with exquisite penthouses, chalets and villas, with unique themes and décor to match the signature lifestyle of each of its inhabitants, replete with private lagoons, health clubs and upscale restaurants. Its newest extension on the same stretch is Telal Soul, perched on the Alexandrian Matrouh Coastal road.</p>
+                <p>Telal El Sokhna, meanwhile, faces the unspoiled crystal waters on the longest beach in Ain El Sokhna, everyone's quick and easy getaway from Cairo's bustle. Its resplendent chalets are carved into the natural elevation of the land and the winding, car-free pathways allow for an immersive connection to nature. In the Cairo metropolis, Stone Park and The Hills New Cairo both are located in prime locations, being navigable from many of the city's hotspots without compromising Roya's sense of tranquility: state-of-the-art apartments, duplexes, penthouses and villas are nestled against a backdrop and greenery.</p>
+                <p>Through its varied destinations, the conscious, integrated design present throughout shows how Roya Developments views each project less as a commodity and more as an aspiration.</p>
 
                 <div className="stats-section">
                     <div className="stats-container" ref={statsRef}>
@@ -92,7 +94,7 @@ function Home() {
                             <h2>{counters.families.toLocaleString()}</h2>
                             <p>NUMBER OF FAMILIES LIVING</p>
                         </div>
-                        <div className="stat-item">
+                        <div className="stat-item stat-last">
                             <a href="/projects" className="learn-more-container">
                                 <img src="/media/arrow.png" alt="Arrow" className="arrow-icon" />
                                 <div className="learn-more-text">
@@ -102,24 +104,23 @@ function Home() {
                             </a>
                         </div>
                     </div>
-                    <div className="eye-image">
-                        <img src="/media/eye.png" alt="Eye" />
-                    </div>
                 </div>
             </div>
 
-            <div className="home03">
-                <div className="home03-content">
-                    <h1>Projects</h1>
-                    <p>Welcome to Roya Development, where visionary design meets unparalleled quality. As a trusted leader in real estate development, we specialize in creating vibrant, sustainable communities that redefine modern living. Our commitment to excellence drives us to craft spaces that seamlessly blend functionality, aesthetics, and innovation, catering to the evolving needs of homeowners and investors alike. With a focus on delivering value and fostering lasting relationships, we take pride in transforming aspirations into reality, one remarkable project at a time.</p>
+            <div className="about02">
+                <div className='mission-container'>
+                    <h1>Mission</h1>
+                    <p>To build on the company's Heritage, develop and deliver innovative sustainable projects that provide our communities with thoughtful living experiences, timeless homes and an unmatched lifestyle.</p>
                 </div>
-            </div>
 
-            <div className="home04">
-                
+                <div className='mission-container'>
+                    <h1>Vision</h1>
+                    <p>We aspire to create inspiring and exquisite communities that enhance quality of life, as well as provide an unmatched combination of luxury, Modernity and convenience in the Egyptian Real estate market.</p>
+                </div>
+
             </div>
 
         </div>
     )
 }
-export default Home;
+export default About;
